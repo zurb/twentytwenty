c25e25910fc969102854ba2d00cc90e23ea831b8
Mark Hayes <mark@zurb.com>
2013-06-10 14:40:50 -0700

    make it easier to use this interaction on touch devices by using the entire container. also ensure that vertical scrolling is not blocked.

--- js/jquery.compareit.js
+++ js/jquery.compareit.js
@@ -1,70 +1,78 @@
 (function($){
 
   $.fn.compareit = function(options) {
-    var sliderPct = 0.5;
-    var container = this;
-    var beforeImg = container.find("img:first");
-    var afterImg = container.find("img:last");
-    container.append("<div class='compareit-slider-bar'></div>");
-    var slider = container.find(".compareit-slider-bar");
-    container.addClass("compareit-container");
-    beforeImg.addClass("compareit-before");
-    afterImg.addClass("compareit-after");
+    return this.each(function() {
+
+      var sliderPct = 0.5;
+      var container = $(this);
+      var beforeImg = container.find("img:first");
+      var afterImg = container.find("img:last");
+      container.append("<div class='compareit-slider-bar'></div>");
+      var slider = container.find(".compareit-slider-bar");
+      container.addClass("compareit-container");
+      beforeImg.addClass("compareit-before");
+      afterImg.addClass("compareit-after");
 
 
-    var calcOffset = function(widthPct) {
-      var w = beforeImg.width();
-      var h = beforeImg.height();
-      return {
-        w: w+"px",
-        h: h+"px",
-        cw: (widthPct*w)+"px"
+      var calcOffset = function(widthPct) {
+        var w = beforeImg.width();
+        var h = beforeImg.height();
+        return {
+          w: w+"px",
+          h: h+"px",
+          cw: (widthPct*w)+"px"
+        };
       };
-    };
 
-    var clipBeforeImg = function(offset) {
-      beforeImg.css("clip", "rect(0,"+offset.cw+","+offset.h+",0)");
-    };
+      var adjustContainer = function(offset) {
+        beforeImg.css("clip", "rect(0,"+offset.cw+","+offset.h+",0)");
+        container.css("height", offset.h);
+      };
 
-    var adjustSlider = function(pct) {
-      var offset = calcOffset(pct);
-      slider.css("height", offset.h);
-      slider.css("left", offset.cw);
-      clipBeforeImg(offset);
-    }
-
-    $(window).on("resize.compareit", function(event) {
-      adjustSlider(sliderPct);
-    });
-
-    $(window).trigger("resize.compareit");
-
-    var offsetX = 0;
-    var imgWidth = 0;
-    
-    slider.on("movestart", function(event) {
-      container.addClass("active");
-      offsetX = container.offset().left;
-      imgWidth = beforeImg.width();
-    });
-
-    container.on("moveend", function(event) {
-      container.removeClass("active");
-    });
-
-    container.on("move", function(event) {
-      if (container.hasClass("active")) {
-        sliderPct = (event.pageX-offsetX)/imgWidth;
-        adjustSlider(sliderPct);
+      var adjustSlider = function(pct) {
+        var offset = calcOffset(pct);
+        slider.css("height", offset.h);
+        slider.css("left", offset.cw);
+        adjustContainer(offset);
       }
-    });
 
-    container.find("img").on("mousedown", function(event) {
-      event.preventDefault();
+      $(window).on("resize.compareit", function(e) {
+        adjustSlider(sliderPct);
+      });
+
+      $(window).trigger("resize.compareit");
+
+      var offsetX = 0;
+      var imgWidth = 0;
+      
+      container.on("movestart", function(e) {
+        if ((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) {
+          e.preventDefault();
+        }
+        container.addClass("active");
+        offsetX = container.offset().left;
+        imgWidth = beforeImg.width();          
+      });
+
+      container.on("moveend", function(e) {
+        container.removeClass("active");
+      });
+
+      container.on("move", function(event) {
+        if (container.hasClass("active")) {
+          sliderPct = (event.pageX-offsetX)/imgWidth;
+          adjustSlider(sliderPct);
+        }
+      });
+
+      container.find("img").on("mousedown", function(e) {
+        event.preventDefault();
+      });
+
     });
 
   };
 
-  // $(".compareit-container").compareit();
+  $(".compareit-container").compareit();
 
 })(jQuery);
