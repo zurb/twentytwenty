77ab4be62ee386da6ee2ec965ef7c0df904c667f
Mark Hayes <mark@zurb.com>
2013-06-10 09:30:50 -0700

    first version of plugin, non-responsive

--- js/jquery.compareit.js
+++ js/jquery.compareit.js
@@ -1,3 +1,35 @@
 (function($){
-  
-})(jQuery)
+
+  $.fn.compareit = function(options) {
+    var offset = this.offset();
+    var bar = this.find(".compareit-bar");
+    var before = this.find(".compareit-image-before");
+    var container = this;
+
+    
+    bar.on("mousedown", function(event){
+      container.addClass("active");
+      var leftOffset = event.pageX - offset.left;
+      bar.css("left", leftOffset + "px");
+      before.css("width", leftOffset + "px");
+    });
+
+    bar.on("mouseup", function(event){
+      container.removeClass("active");
+    });
+
+    container.on("mousemove", function(event){
+        if ($(this).hasClass("active")) {
+          var leftOffset = event.pageX - offset.left;
+          bar.css("left", leftOffset + "px");
+          before.css("width", leftOffset + "px");
+        }
+    });
+
+
+    return this;
+  };
+
+  $("#compare1").compareit();
+
+})(jQuery);
