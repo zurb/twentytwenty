fcb7074997f3aa145d2cf9618d1efe5c814f527e
Nicholas Johnson <Nicholas@Nicholas-MacBook-Pro.local>
2014-02-04 11:38:06 -0600

    Added an option to set the before/after orientation (either horizontal or vertical)

--- README.md
+++ README.md
@@ -24,7 +24,10 @@ $(window).load(function(){
 
 ```js
 $(window).load(function(){
-  $(".twentytwenty-container").twentytwenty({default_offset_pct: 0.7});
+  $(".twentytwenty-container").twentytwenty({
+    default_offset_pct: 0.7, // How much of the before image is visible when the page loads
+    orientation: 'vertical' // Orientation of the before and after images ('horizontal' or 'vertical')
+  });
 });
 ```
 
--- js/jquery.twentytwenty.js
+++ js/jquery.twentytwenty.js
@@ -1,20 +1,24 @@
 (function($){
 
   $.fn.twentytwenty = function(options) {
-    var options = $.extend({default_offset_pct: 0.5}, options);
+    var options = $.extend({default_offset_pct: 0.5, orientation: 'horizontal'}, options);
     return this.each(function() {
 
       var sliderPct = options.default_offset_pct;
       var container = $(this);
+      var sliderOrientation = options.orientation;
+      var beforeDirection = (sliderOrientation === 'vertical') ? 'down' : 'left';
+      var afterDirection = (sliderOrientation === 'vertical') ? 'up' : 'right';
       
-      container.wrap("<div class='twentytwenty-wrapper'></div>");
+      
+      container.wrap("<div class='twentytwenty-wrapper twentytwenty-" + sliderOrientation + "'></div>");
       container.append("<div class='twentytwenty-overlay'></div>");
       var beforeImg = container.find("img:first");
       var afterImg = container.find("img:last");
       container.append("<div class='twentytwenty-handle'></div>");
       var slider = container.find(".twentytwenty-handle");
-      slider.append("<span class='twentytwenty-left-arrow'></span>");
-      slider.append("<span class='twentytwenty-right-arrow'></span>");
+      slider.append("<span class='twentytwenty-" + beforeDirection + "-arrow'></span>");
+      slider.append("<span class='twentytwenty-" + afterDirection + "-arrow'></span>");
       container.addClass("twentytwenty-container");
       beforeImg.addClass("twentytwenty-before");
       afterImg.addClass("twentytwenty-after");
@@ -23,25 +27,30 @@
       overlay.append("<div class='twentytwenty-before-label'></div>");
       overlay.append("<div class='twentytwenty-after-label'></div>");
 
-      var calcOffset = function(widthPct) {
+      var calcOffset = function(dimensionPct) {
         var w = beforeImg.width();
         var h = beforeImg.height();
         return {
           w: w+"px",
           h: h+"px",
-          cw: (widthPct*w)+"px"
+          cw: (dimensionPct*w)+"px",
+          ch: (dimensionPct*h)+"px"
         };
       };
 
       var adjustContainer = function(offset) {
-        beforeImg.css("clip", "rect(0,"+offset.cw+","+offset.h+",0)");
+      	if (sliderOrientation === 'vertical') {
+      	  beforeImg.css("clip", "rect(0,"+offset.w+","+offset.ch+",0)");
+      	}
+      	else {
+          beforeImg.css("clip", "rect(0,"+offset.cw+","+offset.h+",0)");
+    	}
         container.css("height", offset.h);
       };
 
       var adjustSlider = function(pct) {
         var offset = calcOffset(pct);
-        //slider.css("height", offset.h);
-        slider.css("left", offset.cw);
+        slider.css((sliderOrientation==="vertical") ? "top" : "left", (sliderOrientation==="vertical") ? offset.ch : offset.cw);
         adjustContainer(offset);
       }
 
@@ -53,12 +62,17 @@
       var imgWidth = 0;
       
       container.on("movestart", function(e) {
-        if ((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) {
+        if (((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) && sliderOrientation !== 'vertical') {
+          e.preventDefault();
+        }
+        else if (((e.distX < e.distY && e.distX < -e.distY) || (e.distX > e.distY && e.distX > -e.distY)) && sliderOrientation === 'vertical') {
           e.preventDefault();
         }
         container.addClass("active");
         offsetX = container.offset().left;
-        imgWidth = beforeImg.width();          
+        offsetY = container.offset().top;
+        imgWidth = beforeImg.width(); 
+        imgHeight = beforeImg.height();          
       });
 
       container.on("moveend", function(e) {
@@ -67,7 +81,7 @@
 
       container.on("move", function(e) {
         if (container.hasClass("active")) {
-          sliderPct = (e.pageX-offsetX)/imgWidth;
+          sliderPct = (sliderOrientation === 'vertical') ? (e.pageY-offsetY)/imgHeight : (e.pageX-offsetX)/imgWidth;
           if (sliderPct < 0) {
             sliderPct = 0;
           }
